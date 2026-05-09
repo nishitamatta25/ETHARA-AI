8f7257459c1d15da717445d3c41681536a4d7031
nishitamatta25 <mattanishita25@gmail.com>
2026-05-09 16:57:03 +0530

    nn

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','https://ethara-ai-gamma.vercel.app/'],
   credentials: true
 }));
 app.use(express.json());
